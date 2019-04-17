804aeab490e63cc550ba98412499b0ac299ee933
Tai Duy Nguyen <duytai.cse@gmail.com>
2019-04-17 14:18:16 +0800

    IfStatement

--- assets/Sample.sol
+++ assets/Sample.sol
@@ -12,8 +12,18 @@ contract Sample {
     uint k = 100;
     if (val + k > 0) {
       x = val;
+      if (x == val) {
+        x + val;
+      } else {
+        x - val;
+      }
+    } else {
+      x+= val;
     }
     x = x * 2;
+    while(k > 0) {
+      x = x + 3;
+    }
   }
 
   function add(uint step) public {
--- src/graph.rs
+++ src/graph.rs
@@ -4,40 +4,59 @@ use super::walker::{ Walker };
 pub struct Graph<'a> {
     walker: &'a Walker<'a>,
     source: &'a str,
-    root: Option<GraphNode>,
+    root: GraphNode,
 }
 
 #[derive(Debug)]
-pub enum GraphNodeKind {
-    Root,
+pub enum BlockKind {
     Constructor,
-    FunctionBody,
+    BlockBody,
+}
+
+#[derive(Debug)]
+pub enum NodeKind {
+    Root,
+    IfStatement,
 }
 
 #[derive(Debug)]
 pub enum CodeBlock {
     Block(String),
     Link(Box<GraphNode>),
+    None,
 }
 
 #[derive(Debug)]
-pub struct GraphNode {
-    kind: GraphNodeKind,
-    blocks: Vec<CodeBlock>,
+pub enum GraphNode {
+    Standard(Vec<CodeBlock>),
+    IfStatement(IfStatement),
+    None,
 }
+
+#[derive(Debug)]
+pub struct IfStatement {
+    condition: CodeBlock,
+    tblocks: Vec<CodeBlock>,
+    fblocks: Vec<CodeBlock>,
+}
+
 
 impl<'a> Graph<'a> {
     pub fn new(walker: &'a Walker, source: &'a str) -> Self {
-        Graph { walker, source, root: None }
+        Graph { walker, source, root: GraphNode::None }
     }
 
-    pub fn build_block(&mut self, kind: GraphNodeKind, walker: &Walker) -> Vec<CodeBlock> {
+    pub fn build_block(&mut self, kind: BlockKind, walker: &Walker) -> Vec<CodeBlock> {
         let mut blocks = vec![];
         match kind {
-            GraphNodeKind::FunctionBody => {
+            BlockKind::BlockBody => {
                 walker.for_each(|walker, index| {
                     match walker.node.name {
-                        "IfStatement" => {},
+                        "IfStatement" => {
+                            let node = self.build_node(NodeKind::IfStatement, walker); 
+                            let block = CodeBlock::Link(Box::new(node));
+                            blocks.push(block);
+                        },
                         _ => {
                             let from = walker.node.source_offset as usize;
                             let to = from + walker.node.source_len as usize;
@@ -48,7 +67,7 @@ impl<'a> Graph<'a> {
                     }
                 })
             },
-            GraphNodeKind::Constructor => {
+            BlockKind::Constructor => {
                 walker.for_each(|walker, index| {
                     if walker.node.name == "ParameterList" && index == 0 {
                         let from = walker.node.source_offset as usize;
@@ -57,8 +76,8 @@ impl<'a> Graph<'a> {
                         let block = CodeBlock::Block(source.to_string());
                         blocks.push(block);
                     }
-                    if walker.node.name == "Block" && index == 2 {
-                        blocks.append(&mut self.build_block(GraphNodeKind::FunctionBody, walker));
+                    if walker.node.name == "Block" {
+                        blocks.append(&mut self.build_block(BlockKind::BlockBody, walker));
                     }
                 })
             },
@@ -67,12 +86,12 @@ impl<'a> Graph<'a> {
         blocks
     } 
 
-    pub fn build_node(&mut self, kind: GraphNodeKind, walker: &Walker) -> GraphNode {
-        let mut node = GraphNode { kind, blocks: vec![] };
-        match node.kind {
-            GraphNodeKind::Root => {
+    pub fn build_node(&mut self, kind: NodeKind, walker: &Walker) -> GraphNode {
+        match kind {
+            NodeKind::Root => {
                 let mut state_blocks = vec![];
                 let mut constructor_blocks = vec![];
+                let mut blocks = vec![]; 
                 walker.for_each(|walker, _| {
                     if walker.node.name == "ContractDefinition" {
                         walker.for_each(|walker, _| {
@@ -84,7 +103,7 @@ impl<'a> Graph<'a> {
                                 "FunctionDefinition" => {
                                     if is_constructor {
                                         constructor_blocks.append(
-                                            &mut self.build_block(GraphNodeKind::Constructor, walker)
+                                            &mut self.build_block(BlockKind::Constructor, walker)
                                         );
                                     }
                                 },
@@ -99,16 +118,40 @@ impl<'a> Graph<'a> {
                         });
                     }
                 });
-                node.blocks.append(&mut state_blocks);
-                node.blocks.append(&mut constructor_blocks);
-                println!("{:?}", node);
+                blocks.append(&mut state_blocks);
+                blocks.append(&mut constructor_blocks);
+                GraphNode::Standard(blocks)
             },
-            _ => {},
+            NodeKind::IfStatement => {
+                let mut condition = CodeBlock::None; 
+                let mut tblocks = vec![];
+                let mut fblocks = vec![];
+                walker.for_each(|walker, index | {
+                    match walker.node.name {
+                        "BinaryOperation" => {
+                            let from = walker.node.source_offset as usize;
+                            let to = from + walker.node.source_len as usize;
+                            let source = &self.source[from..=to];
+                            condition = CodeBlock::Block(source.to_string());
+                        },
+                        "Block" => {
+                            if index == 1 {
+                                tblocks = self.build_block(BlockKind::BlockBody, walker);
+                            } else {
+                                fblocks = self.build_block(BlockKind::BlockBody, walker);
+                            }
+                        },
+                        _ => {},
+                    }
+                });
+                GraphNode::IfStatement(IfStatement { condition, tblocks, fblocks })
+            },
+            _ => GraphNode::None,
         } 
-        node
     }
 
     pub fn build(&mut self) {
-        self.root = Some(self.build_node(GraphNodeKind::Root, self.walker));
+        self.root = self.build_node(NodeKind::Root, self.walker);
+        println!("{:?}", self.root);
     }
 }
