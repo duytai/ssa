58f8e483e3931f9b818d13c4bb9d0d2aadab8b7a
Tai Duy Nguyen <duytai.cse@gmail.com>
2019-07-15 18:54:46 +0800

    traverse around network

--- src/dfg/network.rs
+++ src/dfg/network.rs
@@ -106,6 +106,81 @@ impl<'a> Network<'a> {
         self.dot.add_links(&self.links);
     }
 
+    /// Find all paths
+    /// keep call_stack for each path to know correct exit point
+    fn find_paths(&'a self, start_at: u32, mut visited: HashSet<u32>, paths: &mut Vec<Vec<&'a DataLink>>, call_stack: Vec<u32>) {
+        let mut targets = vec![];
+        for link in self.links.iter() {
+            if link.get_from() == start_at {
+                match link.get_label() {
+                    DataLinkLabel::InFrom(fc_id) => {
+                        let mut new_call_stack = call_stack.clone();
+                        new_call_stack.push(*fc_id);
+                        targets.push((link, new_call_stack));
+                    },
+                    DataLinkLabel::OutTo(fc_id) => {
+                        if let Some(id) = call_stack.last() {
+                            if id == fc_id {
+                                let mut new_call_stack = call_stack.clone();
+                                new_call_stack.pop();
+                                targets.push((link, new_call_stack));
+                            }
+                        }
+                    },
+                    DataLinkLabel::Internal => {
+                        targets.push((link, call_stack.clone()));
+                    },
+                }
+            }
+        }
+        if !visited.contains(&start_at) && !targets.is_empty() {
+            let prev_paths = paths.clone();
+            paths.clear();
+            visited.insert(start_at);
+            for path in prev_paths {
+                let last_link = path.last().unwrap();
+                if last_link.get_to() == start_at {
+                    for (link, _) in targets.iter() {
+                        let mut new_path = path.clone();
+                        new_path.push(link);
+                        paths.push(new_path);
+                    }
+                } else {
+                    paths.push(path);
+                }
+            }
+            for (link, call_stack) in targets {
+                self.find_paths(link.get_to(), visited.clone(), paths, call_stack);
+            }
+        }
+    }
+
+    /// Traverse around network
+    /// Stop at visited nodes or no more link
+    pub fn traverse(&self, start_at: u32) {
+        let mut paths = vec![];
+        let mut targets: Vec<(&DataLink, Vec<u32>)> = vec![];
+        let mut visited = HashSet::new(); 
+        visited.insert(start_at);
+        for link in self.links.iter() {
+            if link.get_from() == start_at {
+                paths.push(vec![link]);
+                match link.get_label() {
+                    DataLinkLabel::InFrom(fc_id) => {
+                        targets.push((link, vec![*fc_id]));
+                    },
+                    DataLinkLabel::Internal => {
+                        targets.push((link, vec![]));
+                    },
+                    DataLinkLabel::OutTo(_) => {},
+                } 
+            }
+        }
+        for (link, call_stack) in targets {
+            self.find_paths(link.get_to(), visited.clone(), &mut paths, call_stack);
+        }
+    }
+
     pub fn format(&self) -> String {
         self.dot.format()
     }
