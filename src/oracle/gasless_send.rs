3e841d6af22b9174362664b940e449e5899cf443
Tai Duy Nguyen <duytai.cse@gmail.com>
2019-07-15 14:17:30 +0800

    start traverse

--- src/oracle/gasless_send.rs
+++ src/oracle/gasless_send.rs
@@ -1,8 +1,15 @@
 use crate::dfg::Network;
 use crate::core::{
     Shape,
+    Action,
+    Member,
 };
 
+/// Detect gasless send <X>.send() / <X>.transfer()
+/// - address <X> depends on parameters
+///  + has no condition check on path to <X> 
+/// - address <X> depends on msg.sender
+///  + has no condition check on path to <X> 
 pub struct GaslessSend<'a> {
     network: &'a Network<'a>,
 }
@@ -12,15 +19,33 @@ impl<'a> GaslessSend <'a> {
         GaslessSend { network }
     }
 
+    pub fn traverse(&self) {
+        let links = self.network.get_links();
+        // println!("links: {:?}", links);
+    }
+
     pub fn run(&self) -> bool {
         let dfgs = self.network.get_dfgs();
-        let links = self.network.get_links();
+        let send = Member::Global(String::from("send"));
+        let transfer = Member::Global(String::from("transfer"));
         for (_, dfg) in dfgs {
             // Find send / transfer
             let vertices = dfg.get_cfg().get_vertices();
-            for v in vertices {
-                // Function call
-                if v.get_shape() == &Shape::DoubleCircle {
+            let new_actions = dfg.get_new_actions();
+            for vertice in vertices {
+                // Functioncall node
+                if vertice.get_shape() == &Shape::DoubleCircle {
+                    let vertex_id = vertice.get_id();
+                    if let Some(actions) = new_actions.get(&vertex_id) {
+                        for action in actions {
+                            if let Action::Use(var, _) = action {
+                                let members = var.get_members();
+                                if members.contains(&send) || members.contains(&transfer) {
+                                    self.traverse();
+                                }
+                            }
+                        }
+                    }
                 }
             }
         }
