f33f6891413abf50522dcadcddbb5f625faa6fb2
Tai Duy Nguyen <duytai.cse@gmail.com>
2019-09-11 16:15:19 +0800

    save smart contracts

--- src/core/smart_contract.rs
+++ src/core/smart_contract.rs
@@ -7,6 +7,8 @@ pub struct ContractProp {
     states: Vec<u32>,
     functions: Vec<u32>,
     parents: Vec<u32>,
+    indexes: Vec<u32>,
+    calls: Vec<u32>,
 }
 
 #[derive(Debug)]
@@ -21,6 +23,16 @@ pub struct SmartContract {
     contracts: HashMap<u32, Vec<u32>>,
     /// contract_id => vec<state_id> 
     states: HashMap<u32, Vec<u32>>,
+    /// contract_id => vec<index_id>
+    indexes: HashMap<u32, Vec<u32>>,
+    /// contract_id => vec<function_id>
+    calls: HashMap<u32, Vec<u32>>,
+    /// index_id => vec<param_id>
+    idx_params: HashMap<u32, Vec<u32>>,
+    /// call_id => vec<param_id>
+    call_params: HashMap<u32, Vec<u32>>,
+    /// function_id => vec<param_id>
+    func_defs: HashMap<u32, Vec<u32>>,
 }
 
 impl SmartContract {
@@ -28,6 +40,11 @@ impl SmartContract {
         SmartContract {
             contracts: HashMap::new(),
             states: HashMap::new(),
+            indexes: HashMap::new(),
+            calls: HashMap::new(),
+            idx_params: HashMap::new(),
+            call_params: HashMap::new(),
+            func_defs: HashMap::new(),
         }
     }
 
@@ -50,6 +67,8 @@ impl SmartContract {
                 states: vec![],
                 functions: vec![],
                 parents: vec![],
+                indexes: vec![],
+                calls: vec![],
             };
             for walker in contract_walker.direct_childs(|_| true).into_iter() {
                 match walker.node.name {
@@ -63,6 +82,30 @@ impl SmartContract {
                     "FunctionDefinition"
                         | "ModifierDefinition" => {
                         prop.functions.push(walker.node.id);
+                        {
+                            let ig = |_: &Walker, _: &Vec<Walker>| false;
+                            let fi = |walker: &Walker, _: &Vec<Walker>| walker.node.name == "IndexAccess";
+                            for walker in walker.walk(false, ig, fi).into_iter() {
+                                prop.indexes.push(walker.node.id);
+                                let params = walker.direct_childs(|_| true).iter().map(|walker| walker.node.id).collect::<Vec<u32>>();
+                                self.idx_params.insert(walker.node.id, params);
+                            }
+                        }
+                        {
+                            let ig = |_: &Walker, _: &Vec<Walker>| false;
+                            let fi = |walker: &Walker, _: &Vec<Walker>| {
+                                walker.node.name == "FunctionCall" || walker.node.name == "ModifierInvocation"
+                            };
+                            for walker in walker.walk(false, ig, fi).into_iter() {
+                                prop.calls.push(walker.node.id);
+                                let params = walker.direct_childs(|_| true).iter().map(|walker| walker.node.id).collect::<Vec<u32>>();
+                                self.call_params.insert(walker.node.id, params);
+                            }
+                        }
+                        walker.direct_childs(|_| true)
+                            .get(0)
+                            .map(|walker| walker.direct_childs(|_| true).iter().map(|w| w.node.id).collect::<Vec<u32>>())
+                            .map(|ids| self.func_defs.insert(walker.node.id, ids));
                     },
                     "VariableDeclaration" => {
                         prop.states.push(walker.node.id);
@@ -77,6 +120,8 @@ impl SmartContract {
             let mut all_parents = vec![];
             let mut all_functions = vec![];
             let mut all_states = vec![];
+            let mut all_indexes = vec![];
+            let mut all_calls = vec![];
             let mut stacks = vec![*contract_id];
             while !stacks.is_empty() {
                 stacks.pop().and_then(|contract_id| contracts.get(&contract_id)).map(|prop| {
@@ -88,14 +133,22 @@ impl SmartContract {
                 contracts.get(&contract_id).map(|prop| {
                     let mut functions = prop.functions.clone();
                     let mut states = prop.states.clone();
+                    let mut indexes = prop.indexes.clone();
+                    let mut calls = prop.calls.clone();
+
                     all_functions.append(&mut functions);
                     states.reverse();
                     all_states.append(&mut states);
+                    all_indexes.append(&mut indexes);
+                    all_calls.append(&mut calls);
                 });
             }
             all_states.reverse();
             self.contracts.insert(*contract_id, all_functions);
             self.states.insert(*contract_id, all_states);
+            self.indexes.insert(*contract_id, all_indexes);
+            self.calls.insert(*contract_id, all_calls);
         }
+        // Save index access entry
     }
 }
